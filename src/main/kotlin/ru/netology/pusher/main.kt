4aafc067e7ba5395877f4c6e7d1aaf0d83a84fda
Karen Pashoyan <karen-pashoyan@mail.ru>
2024-05-20 20:53:19 +0500

    Initial commit

---
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -0,0 +1,41 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val messageLike = Message.builder()
+        .putData("action", "LIKE")
+        .putData("content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+    val messageNewPost = Message.builder()
+        .putData("action", "NEW_POST")
+        .putData("content", """{
+          "postId": 2,
+          "postAuthor": "Netology",
+          "content": "Знаний хватит на всех: на следующей неделе разбираемся с разработкой мобильных приложений, учимся рассказывать истории и составлять PR-стратегию прямо на бесплатных занятиях "
+          
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(messageLike)
+    FirebaseMessaging.getInstance().send(messageNewPost)
+}
